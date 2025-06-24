bc7080808b0b5894615ddddfae00f4c00c74ab1d
chras731 <136924860+chras731@users.noreply.github.com>
2025-06-23 23:24:47 -0400

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,9 +1,13 @@
 import os
 from supabase import create_client, Client
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_SERVICE_KEY")
-supabase: Client = create_client(url, key)
+def get_supabase_client() -> Client:
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_SERVICE_KEY")
+    print("URL loaded at runtime:", url)
+    print("KEY present?:", bool(key))
+    return create_client(url, key)
+
 
 def insert_videos(video_data):
     response = supabase.table("videos").insert(video_data).execute()
